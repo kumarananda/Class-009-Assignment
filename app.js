46537b2bff18249a10b5bf71039555d5f6c344f2
kumarananda <coderanandakumar@gmail.com>
2021-12-14 01:07:36 +0600

    5EV

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let data3 = JSON.stringify(studentArray);
 let data4 = JSON.stringify(students);
 
 // 001
-//console.log(data1, data2, data3, data4);
+console.log(data1, data2, data3, data4);
 //002
-document.write(data1, data2, data3);
+//document.write(data1, data2, data3);
 
 console.log(`---divider---`);
 
